2014fb0e72f531211f44f603ce31bf3a08aa489a
Fergus Cooper <fcooper8472@gmail.com>
2021-09-20 12:04:27 +0100

    Add all files

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+from distributions import normal_distribution
+
+
+st.title('Statistical distributions')
+
+dist_selector = st.sidebar.selectbox(
+    "Pick a distribution:",
+    ("Beta", "Normal"),
+    index=1
+)
+
+st.header(dist_selector)
+
+if dist_selector == 'Normal':
+    normal_distribution()
